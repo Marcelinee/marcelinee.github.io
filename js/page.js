76909eb0071e37121aa2f657e1b8b8d9ee83903e
Marcelina <eileen.x6@gmail.com>
2017-12-22 01:49:07 +0100

    Transitions for different browsers

--- js/page.js
+++ js/page.js
@@ -1,12 +1,10 @@
     function hoverImg(id) {
         elem = document.getElementById(id);
         sand = document.getElementById("sand");
-        sand.style.transition = "background-image  .3s  ease-in";
         sand.style.backgroundImage = "url('images/" + id + "site.jpg')";
     }
 
     function hoverOutImg() {
         sand = document.getElementById("sand");
         sand.style.backgroundImage = "url('images/sand.jpg')"; 
-        sand.style.transition = "background-image  .2s  ease-in";
     }
